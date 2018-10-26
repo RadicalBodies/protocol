e9e5dd8ff02f218729583077025c36422df62268
Alex Beregszaszi <alex@rtfs.hu>
2018-10-27 00:16:36 +0200

    Add missing interface for ERC721Mintable/MetadataMintable

--- contracts/IProperty.sol
+++ contracts/IProperty.sol
@@ -4,4 +4,7 @@ import "openzeppelin-solidity/contracts/token/ERC721/IERC721Full.sol";
 import "./IERC721WithCreator.sol";
 
 contract IProperty is IERC721Full, IERC721WithCreator {
+    // D'oh. openzeppelin doesn't have an interface for ERC721Mintable/ERC721MetadataMintable.
+    function mintWithTokenUri(address to, uint256 tokenId, string tokenURI) external returns (bool);
+    function burn(uint256 tokenId) external returns (bool);
 }
