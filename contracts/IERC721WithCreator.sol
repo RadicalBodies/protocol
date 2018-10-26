aabcf3a7b559fb5bfd7da2bff6f0e69679f277f3
Alex Beregszaszi <alex@rtfs.hu>
2018-10-26 22:43:27 +0200

    Rename creator(uint256) to creatorOfToken(uint256)

--- contracts/IERC721WithCreator.sol
+++ contracts/IERC721WithCreator.sol
@@ -3,7 +3,7 @@
 // The creator here is the original creator of the given token.
 interface IERC721WithCreator {
     // Returns the original creator of this token.
-    function creator(uint256 tokenId) external view returns (address);
+    function creatorOfToken(uint256 tokenId) external view returns (address);
 
     // Returns the tokenIds of the creator.
     function tokensByCreator(address creator) external view returns (uint256[]);
