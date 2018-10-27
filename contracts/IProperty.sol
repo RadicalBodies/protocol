83d9157c495a32d9c2dfb58d5dcea332bdfe094b
decanus <dean.eigenmann@icloud.com>
2018-10-27 15:42:50 +0200

    added to interface

--- contracts/IProperty.sol
+++ contracts/IProperty.sol
@@ -7,4 +7,5 @@ contract IProperty is IERC721Full, IERC721WithCreator {
     // D'oh. openzeppelin doesn't have an interface for ERC721Mintable/ERC721MetadataMintable.
     function mintWithTokenURI(address to, uint256 tokenId, string tokenURI) external returns (bool);
     function burn(uint256 tokenId) external returns (bool);
+    function move(uint256 tokenId, address to) external returns (bool);
 }
