1898409309b48ee641c810c92e47e88bfdd9a877
Alex Beregszaszi <alex@rtfs.hu>
2018-10-27 14:39:17 +0200

    Add migration for Property/Market

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,1 +1,18 @@
-module.exports = async (deployer) => { };
+const Property = artifacts.require("./Property.sol");
+const Market = artifacts.require("./Market.sol");
+
+module.exports = function (deployer, network) {
+  return deployer.then(async () => {
+    await deployer.deploy(Property);
+    const property = await Property.deployed();
+
+    // interval = 3600 (1 hour)
+    // taxRatePerInterval = 114155 (10% / year)
+    // epsilon = 10000000000000000 (0.01 eth)
+    // beneficiary = 0x0
+    await deployer.deploy(Market, property.address, "3600", "114155", "10000000000000000", "0x0");
+
+    // Transfer token issuance ownership
+    await property.transferOwnership(Market.address);
+  })
+}
