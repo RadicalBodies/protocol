7fb21a8d01824ade3764c64a844021a29e8c492b
Alex Beregszaszi <alex@rtfs.hu>
2018-10-28 11:59:03 +0100

    Add test for calculatePrice

--- test/TestMarket.js
+++ test/TestMarket.js
@@ -22,9 +22,30 @@ contract('Market', function (accounts) {
         await market.register("testURI");
 
         const tokens = await property.tokensByCreator(accounts[0]);
+        // the initial ID is 0
+        assert.equal(tokens[0], 0);
 
         assert.equal(tokens.length, 1);
         assert.equal(await property.tokenURI(tokens[0]), "testURI");
         assert.equal((await market.priceOf(tokens[0])).toString(), "0");
+    });
+
+    it('calculatePrice should work', async () => {
+        await market.register("testURI");
+
+        const epsilon = await market.epsilon();
+        const lastPrice = await market.priceOf(0);
+        const reservePrice = lastPrice.add(epsilon);
+        // 0.01 ETH
+        assert.equal(reservePrice.toString(), "10000000000000000");
+
+        const ret = await market.calculatePrice(0, 1, reservePrice);
+        const periodStart = ret[0];
+        const price = ret[1];
+        const tax = ret[2];
+        // 0.01 ETH
+        assert.equal(price.toString(), "10000000000000000");
+        // 0.0000114155 ETH
+        assert.equal(tax.toString(), "11415500000000");
     });
 });
